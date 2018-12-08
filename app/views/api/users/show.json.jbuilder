bb529a877b0105a7a5fb4de3e281e921ab780f3b
le-s <steven.an.le@gmail.com>
2018-12-07 16:42:36 -0800

    Complete backend of benchbnb

---
+++ app/views/api/users/show.json.jbuilder
@@ -0,0 +1,11 @@
+# entities: {
+#   user: {
+#     1: {
+#       username: stuff,
+#       session_token: iweu,
+#     }
+#   }
+# session: {}
+# }
+
+json.partial! "api/users/user", partial_user: @user
--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { login, signup, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
+  //test
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
+
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
